c8b0a285c44099568ef7239bf76a2e220a4609d4
oheric <ohericyoung@gmail.com>
2015-01-24 18:21:59 -0800

    get request

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.1.8'
 
 gem 'mongoid'
+
+gem 'httparty'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 def index
+	# @search_query = URI.escape(params[:search])
+	@search = JSON.parse(HTTParty.get("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=EricOh5d8-b3ea-46db-b11d-37a78febfe4&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=harry%20potter%20phoenix").body)
 end
 
 end
