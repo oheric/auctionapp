c22268dd1fa9261340fd6f06368d0b11de4fe4f4
oheric <ohericyoung@gmail.com>
2015-01-25 19:06:14 -0800

    format is a grid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,28 @@
+require 'uri'
 class HomeController < ApplicationController
 def index
+	@popular = JSON.parse(HTTParty.get("http://open.api.ebay.com/shopping?callname=FindPopularSearches&responseencoding=JSON&appid=EricOh5d8-b3ea-46db-b11d-37a78febfe4&siteid=0&version=533&CategoryID=15724").body)
+
+
+	
+	@popular['PopularSearchResult'].each do |p|
+	alternative = p['AlternativeSearches'].split(';') 
+	related = p['RelatedSearches'].split(';') 
+	@keywords = alternative.concat(related) 
 	if params[:price]
-	search_query = URI.escape(params[:price])
-	@item = JSON.parse(HTTParty.get("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=EricOh5d8-b3ea-46db-b11d-37a78febfe4&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=15724&itemFilter.name=MaxPrice&itemFilter.value=" + search_query.to_s + "&itemFilter.paramName=Currency&itemFilter.paramValue=USD&paginationInput.entriesPerPage=9").body)
+		search_query = URI.escape(params[:price])
+		@item = JSON.parse(HTTParty.get("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=EricOh5d8-b3ea-46db-b11d-37a78febfe4&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + URI.encode(@keywords.sample) + "&categoryId=15724&itemFilter.name=MaxPrice&itemFilter.value=" + search_query.to_s + "&itemFilter.paramName=Currency&itemFilter.paramValue=USD&itemFilter.name=HideDuplicateItems&itemFilter.value=true&paginationInput.entriesPerPage=12").body)
+
+end
+
+		# @item = JSON.parse(HTTParty.get("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=EricOh5d8-b3ea-46db-b11d-37a78febfe4&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=15724&itemFilter.name=MaxPrice&itemFilter.value=" + search_query.to_s + "&itemFilter.paramName=Currency&itemFilter.paramValue=USD&paginationInput.entriesPerPage=9").body)
+	
 
 
 
-	# @item = JSON.parse(HTTParty.get("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=EricOh5d8-b3ea-46db-b11d-37a78febfe4&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=harry%20potter%20phoenix&itemFilter.name=MaxPrice&itemFilter.value=" + search_query.to_s + "&itemFilter.paramName=Currency&itemFilter.paramValue=USD&paginationInput.entriesPerPage=3").body)
+
+
+	
 end
 end
 
