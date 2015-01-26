d0c6841f7bffd0267b7aa687bf528d1d4ac185d0
oheric <ohericyoung@gmail.com>
2015-01-25 22:59:13 -0800

    set time remaining to 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,7 @@ def index
 	@keywords = alternative.concat(related) 
 	if params[:price]
 		search_query = URI.escape(params[:price])
-		@item = JSON.parse(HTTParty.get("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=EricOh5d8-b3ea-46db-b11d-37a78febfe4&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + URI.encode(@keywords.sample) + "&categoryId=15724&itemFilter.name=MaxPrice&itemFilter.value=" + search_query.to_s + "&itemFilter.paramName=Currency&itemFilter.paramValue=USD&itemFilter.name=HideDuplicateItems&itemFilter.value=true&paginationInput.entriesPerPage=12").body)
+		@item = JSON.parse(HTTParty.get("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=EricOh5d8-b3ea-46db-b11d-37a78febfe4&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + URI.encode(@keywords.sample) + "&categoryId=15724&itemFilter(0).name=MaxPrice&itemFilter(0).value=" + search_query + "&itemFilter(1).paramName=Currency&itemFilter(1).paramValue=USD&itemFilter(2).name=HideDuplicateItems&itemFilter(2).value=true&itemFilter(3).name=EndTimeTo&itemFilter(3).value=" + (DateTime.now + 5.minutes).to_s + "&paginationInput.entriesPerPage=12").body)
 
 end
 
