a67d0379c7e4a030a1d48e2d972fce920dc33947
oheric <ohericyoung@gmail.com>
2015-02-03 22:13:23 -0800

    grid

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'wclothing/index' => 'wclothing#index', as: :wclothing
+
+  get 'home/show' => 'home#show', as: :homeshow
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
